34ef58101b10ec11ef984cd309cdab78867a2c28
ant-7802 <anttubemc@googlemail.com>
2023-01-12 18:53:21 +0000

    addauto updating thingy

--- javascript/index.js
+++ javascript/index.js
@@ -5,3 +5,20 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+document.getElementsByClassName("navbar-nav")[0].innerHTML += `
+<li class="nav-item">
+<h1 class="fach_title">Fach Juegos!&nbsp-&nbsp</h1>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Home</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Games</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="about.html">About</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Contact</a>
+</li>
+`
